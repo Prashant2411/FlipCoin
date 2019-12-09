55c678f0a630c205ae672a1bdf35948f8606d30d
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-09 16:24:17 +0530

    Added: Calculating percentage of heads and tails

--- FlipCoin/flipCoinStimulator.sh
+++ FlipCoin/flipCoinStimulator.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -x
 
+HEAD=0
+TAIL=0
+
+declare -A flipResultDict
+
 echo "Welcome to the Flip Coin Stimulator"
 
-coinFace=$(( RANDOM%2 ))
-case $coinFace in
-        1 )
-                echo "Head";;
-        0 )
-                echo "Tail";;
+for (( i=0;i<10;i++ ))
+do
+	coinFace=$(( RANDOM%2 ))
+	case $coinFace in
+        	1 )
+                	flipResultDict[((Flip$i))]="Head"
+			head=$(( $head + 1 ));;
+        	0 )
+			flipResultDict[((Flip$i))]="Tail"
+			tail=$(( $tail + 1 ));;
 esac
+done
+
+echo "Head Percent: $(( $head * 10 ))"
+echo "Tail Percent: $(( $tail * 10 ))"
