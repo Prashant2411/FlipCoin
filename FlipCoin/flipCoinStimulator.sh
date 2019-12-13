cb7ea64595a260281fff949046e36b2429b661d4
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-13 15:24:07 +0530

    Added: Declared a variable NO_OF_TIMES_FLIPPED

--- FlipCoin/flipCoinStimulator.sh
+++ FlipCoin/flipCoinStimulator.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -x
 
+NO_OF_TIMES_FLIPPED=10
+
 echo "Welcome to the Flip Coin Stimulator"
 
 declare -A flipResultDict
 
 function getFlipResult () {
-	for (( i=0;i<$FLIP_NUMBER;i++ ))
+	for (( i=0;i<$NO_OF_TIMES_FLIPPED;i++ ))
 	do
 		unset res
 		for (( j=0;j<$1;j++ ))
@@ -18,7 +20,7 @@ function getFlipResult () {
 						res="T$res";;
 			esac
 		done
-		flipResultDict[$res]=$(( ${flipResultDict[$res]} + 1 * 100 / 10 ))
+		flipResultDict[$res]=$(( ${flipResultDict[$res]} + 1 * 100 / $NO_OF_TIMES_FLIPPED ))
 	done
 }
 
